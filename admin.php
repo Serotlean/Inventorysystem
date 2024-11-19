983fba5cf336ef8c4c04ae97aa880e29f707ef92
SerotLean <leanserot20@gmail.com>
2024-11-19 18:58:25 +0800

    INital con

--- admin.php
+++ admin.php
@@ -4,6 +4,7 @@
   // Checkin What level user has permission to view this page
    page_require_level(1);
 ?>
+<!-- dnadawdhawdh -->
 <?php
  $c_categorie     = count_by_id('categories');
  $c_product       = count_by_id('products');
